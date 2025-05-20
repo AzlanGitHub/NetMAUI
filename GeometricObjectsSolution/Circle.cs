da295601010e9d5c1cf63a35344ba5878cb21960
CC-Student <azlanainto97@gmail.com>
2025-05-20 15:58:42 +0200

    Projektdateien hinzufügen.

---
+++ GeometricObjectsSolution/Circle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjectsSolution
+{
+    public class Circle
+    {
+        //Objektvariablen
+        // Cirle kreis;
+
+        // kreis = new Circle();
+        // Circle kreis = new Circle();
+        // Circle kreis1, kreis2, kreis3;
+        // Circle kreis1, kreis2, kreis3 = new Circle();
+        private double _YCoordinate;
+        public double YCoordinate
+        {
+            get { return _YCoordinate; }
+            set { _YCoordinate = value; }
+        }
+        private double _XCoordinate;
+        public double XCoordinate
+        {
+            get { return _XCoordinate; }
+            set { _XCoordinate = value; }
+        }
+        private double _Radius;
+        public double Radius
+        {
+            get { return _Radius; }
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Der Radius muss größer als 0 sein!");
+                    
+                }
+                _Radius = value; 
+            }
+        }
+
+        public double GetPerimeter()
+        {
+                       return 2 * Math.PI * Radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.Pow(Radius, 2) * Math.PI;
+        }
+        public Circle(double xCoordinate, double yCoordinate, double radius)
+        {
+            XCoordinate = xCoordinate;
+            YCoordinate = yCoordinate;
+            Radius = radius;
+        }
+
+        public static void Main(string[] args)
+        {
+            Circle kreis = new Circle(0, 0, 5);
+            Console.WriteLine($"Der Umfang des Kreises beträgt: {kreis.GetPerimeter()}");
+            Console.WriteLine($"Die Fläche des Kreises beträgt: {kreis.GetArea()}");
+            Console.WriteLine(args[0]);
+           // Console.WriteLine(args[1]);
+        }
+    }
+}
---
+++ GeometricObjectsSolution/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GeometricObjectsSolution;
+
+Console.WriteLine("Hello, World!");
+ string[] a = Environment.GetCommandLineArgs();
+Circle.Main(a);
